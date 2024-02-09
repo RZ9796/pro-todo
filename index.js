99034d7e7d7b98f5623b181cc1797a1752632159
RZ9796 <rushizalte2001@gmail.com>
2024-02-09 19:00:29 +0530

    ui  commit

--- index.js
+++ index.js
@@ -5,10 +5,15 @@ require("dotenv").config({ path: "./.env" });
 mongoose.connect(process.env.MONGO_URL);
 const app = express();
 
+const path = require("path");
+app.use(express.static(path.join(__dirname, "dist"))); //
 app.use(express.json());
 app.use(cors());
 // route
 app.use("/api/admin", require("./routes/todoRoute"));
+app.use("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "dist", "index.html"));
+});
 
 mongoose.connection.once("open", () => {
   console.log("mongo connected");
